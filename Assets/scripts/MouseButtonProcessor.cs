7d3e6bfd85bcd10ddff673d98e0db29a5573eebe
puglisac <alan.c.puglisi@gmail.com>
2020-12-16 15:00:22 -0600

    exercise complete

--- Assets/scripts/MouseButtonProcessor.cs
+++ Assets/scripts/MouseButtonProcessor.cs
@@ -22,8 +22,57 @@ public class MouseButtonProcessor : MonoBehaviour
 	void Update()
 	{
         // spawn teddy bear as appropriate
+        if(Input.GetAxis("SpawnTeddyBear")>0)
+        {
+            if (!spawnInputOnPreviousFrame)
+            {
+                spawnInputOnPreviousFrame = true;
+
+                //get location of mouse on click
+                Vector3 newPosition = Input.mousePosition;
+                newPosition.z = -Camera.main.transform.position.z;
+                newPosition = Camera.main.ScreenToWorldPoint(newPosition);
+                //spawn new teddyBear
+                GameObject teddyBear = Instantiate(prefabTeddyBear) as GameObject;
+                teddyBear.transform.position = newPosition;
+            }
+        }
+        else
+        {
+            spawnInputOnPreviousFrame = false;
+        }
 
         // explode teddy bear as appropriate
-		
-	}
+        if (Input.GetAxis("ExplodeTeddyBear") > 0)
+        {
+            if (!explodeInputOnPreviousFrame)
+            {
+                explodeInputOnPreviousFrame = true;
+
+                GameObject teddyBearToExplode = GameObject.FindWithTag("TeddyBear");
+                if (teddyBearToExplode)
+                {
+                    Vector3 location = teddyBearToExplode.transform.position;
+                    GameObject explosion = Instantiate(prefabExplosion) as GameObject;
+                    explosion.transform.position = location;
+                    Destroy(teddyBearToExplode);
+                }
+            
+            }
+        }
+        else
+        {
+            explodeInputOnPreviousFrame = false;
+        }
+
+    }
 }
+
+
+//Vector3 newPosition = Input.mousePosition;
+//newPosition.z = -Camera.main.transform.position.z;
+//newPosition = Camera.main.ScreenToWorldPoint(newPosition);
+//transform.position = newPosition;
+
+//GameObject teddyBear = Instantiate(prefabTeddyBear) as GameObject;
+//teddyBear.transform.position = worldLocation;
